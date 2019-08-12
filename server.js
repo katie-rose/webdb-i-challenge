d042e1f3a345689cd4ebf4e9ca06b11ffc30164f
Katie Rose <katiefelten@gmail.com>
2019-08-12 16:29:10 -0400

    :rocket: GET id, PUT id, DELETE id, and POST new account working

--- server.js
+++ server.js
@@ -22,7 +22,51 @@ server.get("/:id", (req, res) => {
       res.status(200).json(account);
     })
     .catch(error => {
-      res.status(500).json({ error: "Error getting the account from database" });
+      res
+        .status(500)
+        .json({ error: "Error getting the account from database" });
+    });
+});
+
+server.post("/", (req, res) => {
+  const post = req.body;
+  db("accounts")
+    .insert(post, "id")
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Error retrieving accounts from database" });
+    });
+});
+
+server.put("/:id", (req, res) => {
+  db("accounts")
+  .where({ id: req.params.id})
+  .update(req.body)
+    .then(newPut => {
+      res.status(200).json(newPut);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "You dun goofed" });
+    });
+});
+
+server.delete("/:id", (req, res) => {
+  db("accounts")
+  .where("id", req.params.id)
+  .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "You dun goofed" });
     });
 });
 
