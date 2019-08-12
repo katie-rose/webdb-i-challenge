b81f768538b29f38a4de2cecfb506b2c1bef7d95
Katie Rose <katiefelten@gmail.com>
2019-08-12 15:38:41 -0400

    :memo: updated queries answers

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require('express');
-const db = require('./data/dbConfig.js');
+const express = require("express");
+const db = require("./data/dbConfig.js");
 const server = express();
 server.use(express.json());
 
 server.get("/", (req, res) => {
   db("accounts")
-    .then(accounts => {
-      res.status(200).json(accounts);
+    .then(account => {
+      res.status(200).json(account);
     })
     .catch(error => {
       res
         .status(500)
         .json({ message: "Error retrieving accounts from database" });
+    });
+});
+
+server.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Error getting the account from database" });
     });
 });
 
