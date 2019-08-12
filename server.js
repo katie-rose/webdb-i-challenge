297d3d7bf91b3626e1ede2440f2eebb1d6aba011
Katie Rose <katiefelten@gmail.com>
2019-08-12 15:10:17 -0400

    :rocket: added GET root and recieved a 200 response

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
-
 const server = express();
-
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Error retrieving accounts from database" });
+    });
+});
 
 module.exports = server;
